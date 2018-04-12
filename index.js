21a39157bc90cce5598cb1ddab688c7cdba0b544
Jeremy Su <jeremycksu@yahoo.com>
2018-04-11 20:42:08 -0400

    implemented dummy tracker

--- index.js
+++ index.js
@@ -10,9 +10,19 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+
+var tracker = {};
+tracker.declaration = ["127.0.0.1:9000", "192.68.103.2:8000"];
+tracker.movie = ["130.64.155.2:3000"];
+
 app.get('/', function(request, response) {
-  response.render('pages/index')
+  response.send(tracker);
 });
+
+app.post('/join', function (request, response) {
+  var client_ip = request.body;
+  response.send(request);
+})
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
