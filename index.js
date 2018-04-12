617789253d11739f49ffbc842cb225843682c625
Jeremy Su <jeremycksu@yahoo.com>
2018-04-11 20:28:46 -0400

    Setting up heroku

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cool = require('cool-ascii-faces');
+
 var express = require('express');
 var app = express();
 
@@ -12,10 +12,6 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index')
-});
-
-app.get('/cool', function(request, response) {
-  response.send(cool());
 });
 
 app.listen(app.get('port'), function() {
