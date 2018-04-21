77a376517b7ad37dae4fbc34a069885de3fe55af
Bhushan Suwal <bhushan_suwal@live.com>
2018-04-21 16:28:47 -0400

    cleanups

--- client/chunkHandler.py
+++ client/chunkHandler.py
@@ -5,26 +5,21 @@ class ChunkHandler:
     def __init__(self):
         self.chunkSize = CHUNKSIZE # in bytes
         self.up_chunks = [] # contains uploading chunks
-        self.dl_chunk_ids = [] # contains downloading chunks
-        self.dl_chunk_map = {}
+
+        self.dl_chunk_ids = [] # contains downloading chunk ids
+        self.dl_chunk_map = {} # id : chunk
         self.dl_total_num_chunks = 0
 
+    def get_chunk_size(self):
+        return self.chunkSize
+
+    # DOWNLOADER FUNCIONS
     def set_dl_chunk(self, chunk_id, chunk):
         self.dl_chunk_map[chunk_id] = chunk
 
-    def get_num_up_chunks(self):
-        return len(self.up_chunks)
-
-    def get_chunk(self, index):
-        if index >= self.get_num_up_chunks():
-            sys.stderr.write("Chunk index out of range\n")
-        return self.up_chunks[index]
-
-    # DOWNLOADER FUNCIONS
     def stitch_chunks(self):
         ''' Returns all the chunks in the chunk array sorted and stitched
             as one large array'''
-        # TODO: untested
         sorted_ids = sorted(self.dl_chunk_ids)
         chunks = []
         for id in sorted_ids:
@@ -32,11 +27,18 @@ class ChunkHandler:
         return "".join(chunks)
 
     # UPLOADER FUNCTIONS
+    def get_chunk(self, index):
+        if index >= self.get_num_up_chunks():
+            sys.stderr.write("Chunk index out of range\n")
+        return self.up_chunks[index]
+
+    def get_num_up_chunks(self):
+        return len(self.up_chunks)
+
     def split_file_to_chunks(self, file):
         ''' Takes in a file, opens it and splits it into chunks, and
             loads them into the chunk array.
             Assumes file is already uu.encoded and is a .txt file'''
-        # TODO : untested
         with open(file) as f:
             chunk = "placeholder to make len(chunk) != 0"
             while len(chunk) != 0:
@@ -50,8 +52,6 @@ class ChunkHandler:
               as a string delimted by '-' eg. 1-45-67-22 
             * Length of the string  
             '''  
-
-        # TODO: currently just evens
         chunk_list = []
         num_chunks = self.get_num_up_chunks()
         for i in range(0, num_chunks, 2):
@@ -67,8 +67,6 @@ class ChunkHandler:
               as a string delimted by '-' eg. 1-45-67-22 
             * Length of the string  
             ''' 
-
-        # TODO: currently just evens
         chunk_list = []
         num_chunks = self.get_num_up_chunks()
         for i in range(1, num_chunks, 2):
--- client/server.py
+++ client/server.py
@@ -14,13 +14,11 @@ class Server:
     def __init__(self, host_name):
         self.host_name = host_name
         self.port = random.randint(9000, 9100)
-        self.chunk_size = CHUNKSIZE
 
         # make a directory to store seeding files
         self.files_dir = 'server_files'
         os.system('mkdir ' + self.files_dir)
         self.create_seed_socket()
-        self.ch = ChunkHandler()
 
     def get_port(self):
         return self.port
@@ -32,9 +30,8 @@ class Server:
     def create_seed_socket(self):
         ''' Creates a socket for the server and listens at the port'''
 
-        # create socket
+        # create socket, remove Address in Use error
         self.up_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # remove Address in Use error
         self.up_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         
         # find a port number to bind to
@@ -57,72 +54,60 @@ class Server:
         ''' Runs the server '''
         while True:
             # listen for incoming requests
-            clientsocket,addr = self.up_socket.accept()      
+            sock, addr = self.up_socket.accept()      
             print("Got a connection from %s" % str(addr))
             threading.Thread(target=self.handle_client, 
-                args=(clientsocket, addr)).start()
+                args=(sock, addr)).start()
 
-    def handle_client(self, clientsocket, address):
+    def handle_client(self, sock, address):
         """
             Sends the leech the available packets we have to seed
             and then waits for response
         """
         PACKET_SIZE = 1024
         CRLF = "\r\n"
+        ch = ChunkHandler() # declare a chunk handler for the upload
 
         # Recieve request of file from client, extract filename
-        cli_data = clientsocket.recv(PACKET_SIZE).decode() 
+        cli_data = sock.recv(PACKET_SIZE).decode() 
         if (cli_data[:6] == "GET / "):
             filename = cli_data.split('Filename: ')[1].split('Downloader: ')[0].split(CRLF)[0]
             print("Got a GET request for {file}".format(file=filename))
 
-            length = self.send_chunk_list(filename, clientsocket)
+            total_chunks = self.send_chunk_list(ch, filename, sock)
+            self.seed_file(ch, total_chunks, sock)
+            sock.close()
 
-            # self.seed_file(filename, clientsocket)
-            for i in range(length):
-                req = clientsocket.recv(4)
-                if (len(req) == 0):
-                    break
-                id = struct.unpack('>I', req[:4])[0]
-                chunk = self.ch.get_chunk(id)
-                clientsocket.send(chunk.encode('ascii'))
-
-            clientsocket.close()
-
-    def send_chunk_list(self, filename, clientsocket):
-        ''' Opens file, determines the number of available chunks
-            to seed, and sends that list to the socket'''
+    def send_chunk_list(self, ch, filename, sock):
+        ''' ch : upload chunk handler object
+            Opens file, determines the number of available chunks
+            to seed, and sends :
+                1. number of total packets in file
+                2. number of bytes in list
+                3. list as a string '''
         
-        self.ch.split_file_to_chunks(self.files_dir + "/" + filename + ".txt")
-        num_bytes, data = self.ch.get_chunk_ids_even()
+        ch.split_file_to_chunks(self.files_dir + "/" + filename + ".txt")
+        num_bytes, data = ch.get_chunk_ids_odd()
     
         # send total number in complete file
-        length = self.ch.get_num_up_chunks()
+        length = ch.get_num_up_chunks()
         encoded_length = struct.pack('>I', length)
         encoded_bytes = struct.pack('>I', num_bytes)
 
-        clientsocket.send(encoded_length) 
-        clientsocket.send(encoded_bytes) 
-        clientsocket.send(data.encode('ascii')) # send actual delimited data
+        sock.send(encoded_length) 
+        sock.send(encoded_bytes) 
+        sock.send(data.encode('ascii')) # send actual delimited data
+
         return length
 
-    def seed_file(self, filename, clientsocket):
-        num_chunks = self.ch.get_num_chunks()
-        for i in range(0, num_chunks, 2):
-            chunk = self.ch.get_chunk(i)
-            self.send_msg(resp, self.utf8len(chunk), clientsocket)
-
-    def send_msg(self, resp, resp_len, clientsocket):
-        totalsent = 0
-        while totalsent < resp_len:
-            sent = clientsocket.send(resp[totalsent:].encode('ascii'))
-            if sent == 0:
-                raise RuntimeError("socket connection broken")
-            totalsent = totalsent + sent
-
-    def utf8len(self, s):
-        ''' Returns the size of a string in bytes'''
-        return len(s.encode('utf-8'))
+    def seed_file(self, ch, total_chunks, sock):
+        for i in range(total_chunks):
+            req = sock.recv(4)
+            if (len(req) == 0):
+                break
+            id = struct.unpack('>I', req[:4])[0]
+            chunk = ch.get_chunk(id)
+            sock.send(chunk.encode('ascii'))
 
     #########################################################################
     #                    DOWNLOADING SERVER FUNCTIONS                       #
@@ -145,101 +130,75 @@ class Server:
         host2 = ips[1].split(":")[0]
         port2 = int(ips[1].split(":")[1])  
 
-        self.dl_ch = ChunkHandler()  
+        ch = ChunkHandler()  
 
         threading.Thread(target=self.set_peer_conn, 
-                args=(host1, port1, filename)).start()
+                args=(host1, port1, filename, ch)).start()
 
         threading.Thread(target=self.set_peer_conn, 
-                args=(host2, port2, filename)).start()
+                args=(host2, port2, filename, ch)).start()
 
-    def set_peer_conn(self, host, port, filename):
-        CRLF = "\r\n"
-        # create a socket object
+    def set_peer_conn(self, host, port, filename, ch):
+        ''' ch: download chunk handler object '''
+        
+        # create a socket object, remove Address in use error
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-        # remove Address in use error
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.connect((host, port))         
 
+        self.send_file_req(filename, sock)  
+        chunk_ids = self.receive_chunk_ids(sock, ch)
+
+        # add each chunk to the download list and map
+        for id in chunk_ids:
+            chunk = self.request_chunk(int(id), sock, ch)
+            print("Received chunk: {id}".format(id = id))
+            ch.dl_chunk_map[int(id)] = chunk
+            ch.dl_chunk_ids.append(int(id))
+
+        # if download is complete, save file as a txt file and then decode it
+        if (len(ch.dl_chunk_ids) == ch.total_num_chunks):
+            final_file = ch.stitch_chunks()
+            with open(self.torr_dir + '/' + filename + ".txt", 'w') as f:
+                f.write(final_file)
+            uu.decode(self.torr_dir + "/" + filename + ".txt", 
+                self.torr_dir + "/" + filename)
+
+
+        print(len(ch.dl_chunk_ids)) 
+        print(ch.total_num_chunks)
+
+        sock.close()
+
+    def send_file_req(self, filename, sock):
+        ''' Sends initial request to seeder about the file
+            we want to download'''
+        CRLF = "\r\n"
         msg = "GET / HTTP/1.1" + CRLF \
                 + "Filename: " + filename + CRLF \
                 + "Downloader: " + self.host_name + CRLF \
                 + "Port: " + str(self.port) + CRLF + CRLF
         sock.send(msg.encode('ascii'))
-        
-        first = sock.recv(4)
-        second = sock.recv(4)
-        total_num_chunks = struct.unpack('>I', first[:4])[0]
-        num_bytes = struct.unpack('>I', second[:4])[0]
 
-        self.dl_ch.total_num_chunks = total_num_chunks
-        chunk_ids = self.receive_chunk_ids(sock, num_bytes)
-
-        print(chunk_ids)
-        for id in chunk_ids:
-            chunk = self.request_chunk(int(id), sock)
-            print("Received chunk: {id}".format(id = id))
-            self.dl_ch.dl_chunk_map[int(id)] = chunk
-            self.dl_ch.dl_chunk_ids.append(int(id))
-
-        if (len(self.dl_ch.dl_chunk_ids) == self.dl_ch.total_num_chunks):
-            final_file = self.build_chunk_list()
-
-            # store the file as a txt file and then decode it
-            with open(self.torr_dir + '/' + filename + ".txt", 'w') as f:
-                f.write(final_file)
-            
-            uu.decode(self.torr_dir + "/" + filename + ".txt", 
-                self.torr_dir + "/" + filename)
-
-
-        print(len(self.dl_ch.dl_chunk_ids)) 
-        print(self.dl_ch.total_num_chunks)
-
-        sock.close()
-
-    def build_chunk_list(self):
-        return self.dl_ch.stitch_chunks()
-
-    def request_chunk(self, id, sock):        
+    def request_chunk(self, id, sock, ch):        
         # send request for chunk
         print("Requesting chunk: {id}".format(id = id))
         msg = struct.pack('>I', id)
         sock.send(msg)
         # get chunk back
-        return sock.recv(self.chunk_size).decode('ascii')
+        return sock.recv(ch.get_chunk_size()).decode('ascii')
 
-    def receive_chunk_ids(self, sock, num_bytes):
-        ''' First recieves the number of bytes in data, 
-            then recieves the actual chunk_id data, parses it and
-            returns as a list of numbers'''
+    def receive_chunk_ids(self, sock, ch):
+        ''' Recieves the total number of chunks in the file,
+            the total number of bytes in the chunk-list string
+            and then the chunk-list string. Parses the
+            chunk-list and returns a list of chunk-ids '''
+        total_chunks = sock.recv(4)
+        total_chunks = struct.unpack('>I', total_chunks[:4])[0]
+
+        num_bytes = sock.recv(4)
+        num_bytes = struct.unpack('>I', num_bytes[:4])[0]
+
+        ch.total_num_chunks = total_chunks
         chunk_list = sock.recv(num_bytes).decode('ascii').split('-')
-
         return chunk_list
-
-    def receive_torrented_file(self, sock, filename):
-        ''' 
-        '''       
-        # recieve the actual file as a bytes object
-        file = self.receive_msg(sock)        
-
-        # store the file as a txt file and then decode it
-        with open(self.torr_dir + '/' + filename + ".txt", 'w') as f:
-            f.write(file.decode('ascii'))
-        
-        uu.decode(self.torr_dir + "/" + filename + ".txt", 
-            self.torr_dir + "/" + filename)
-
-    # def receive_msg(self, sock):
-    #     ''' Recieves message from socket chunk by chunk and returns 
-    #         complete message as a bytes object'''
-    #     chunks = []
-    #     bytes_recd = 0
-    #     while bytes_recd < file_len:
-    #         chunk = sock.recv(min(file_len - bytes_recd, 8192))
-    #         if chunk == b'':
-    #             raise RuntimeError("socket connection broken")
-    #         chunks.append(chunk)
-    #         bytes_recd = bytes_recd + len(chunk)
-    #     return b''.join(chunks)
-    #     
