b0dcdf9d6a1e038caa4eb89dc931bd2e49601ad4
Bhushan Suwal <bhushan_suwal@live.com>
2018-04-29 21:48:47 -0400

    updated README.md

--- demo1.sh
+++ demo1.sh
@@ -3,4 +3,3 @@ xterm -title "Node server" -hold -e "node index.js" &
 sleep 5
 xterm -title "Peer 1 - Every Chunk" -hold -e "cd client; python3 client.py u 2 1" &
 xterm -title "Peer 2 - Odd Chunks" -hold -e "cd client; python3 client.py u 3 1" &
-
--- demo2.sh
+++ demo2.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 xterm -title "Node server" -hold -e "node index.js" &
 sleep 5
+xterm -title "Peer 2 - Initially slower" -hold -e "cd client; python3 client.py u 20 2" &
+sleep 3
 xterm -title "Peer 1 - Fast, then slow" -hold -e "cd client; python3 client.py u 0 2" &
-xterm -title "Peer 2 - Initially slower" -hold -e "cd client; python3 client.py u 5 2" &
+
+
 
